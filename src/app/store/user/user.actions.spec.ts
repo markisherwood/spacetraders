e46cc514e512ed675797e6d35fe94f63c20e861d
Mark Isherwood <Mark.Isherwood@unisuper.com.au>
2021-04-30 17:26:59 +1000

    WIP Testing of effects.

--- src/app/store/user/user.actions.spec.ts
+++ src/app/store/user/user.actions.spec.ts
@@ -3,21 +3,21 @@ import * as fromUser from './user.actions';
 
 describe('loginUser', () => {
   it('should return an action', () => {
-    expect(fromUser.registerUser({username: 'test'}).type).toBe('[User] Register User');
+    expect(fromUser.registerUser({username: 'test'}).type).toBe('[Login Component] Register User');
   });
   it('should return an action', () => {
-    expect(fromUser.loginUser({username: 'test', token: 'asfd'}).type).toBe('[User] Login User');
+    expect(fromUser.loginUser({username: 'test', token: '1234'}).type).toBe('[Login Component] Login User');
   });
   it('should return an action', () => {
     const user: User = {
       username: 'test',
       credits: 0,
       loans: [],
-      ships: [],      
+      ships: [],
     }
-    expect(fromUser.loadUserSuccess({user, token: 'asdf'}).type).toBe('[User] Load User Success');
+    expect(fromUser.loadUserSuccess({user, token: '1234'}).type).toBe('[User Service] Load User Success');
   });
   it('should return an action', () => {
-    expect(fromUser.loadUserFailure({error: 'Login failed'}).type).toBe('[User] Load User Failure');
+    expect(fromUser.loadUserFailure({error: 'Login failed'}).type).toBe('[User Service] Load User Failure');
   });
 });
--- src/app/store/user/user.effects.spec.ts
+++ src/app/store/user/user.effects.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { UserService } from '@core/services/user.service';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { Observable } from 'rxjs';
-import { Action } from 'rxjs/internal/scheduler/Action';
+import { Action } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+
 
 import { UserEffects } from './user.effects';
 
+type Spied<T> = {
+  [Method in keyof T]: jasmine.Spy;
+}
+
 describe('UserEffects', () => {
   let actions$: Observable<Action>;
+  let userService: Spied<UserService> = jasmine.createSpyObj('userService', [
+    'register',
+    'loadUser'
+  ]);
   let effects: UserEffects;
+  let testScheduler: TestScheduler;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,6 +30,18 @@ describe('UserEffects', () => {
     });
 
     effects = TestBed.inject(UserEffects);
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
   });
 
+  describe('RegisterUser Effect', () => {
+    it('Should generate success action', () => {
+
+      testScheduler.run(({hot, cold, expectObservable}) => {
+        actions$ = hot<Action>('-a', { 'a': { type: '[Login Component] Register User'}});
+        userService.register.and.returnValue(cold('--a', { a: { response: 'success'}}));
+      });
+    });
+  });
 });
